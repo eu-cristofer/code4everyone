5e6b160d1392fe7bbc5dc59454b36d4961f1b399
Cristofer Costa <cristofercosta@yahoo.com.br>
2023-03-10 16:08:56 -0300

    lists

--- 13_ADT_list.c
+++ 13_ADT_list.c
@@ -31,6 +31,43 @@ list* add_to_front(int d, list *h)
     return head;
 }
 
+list* array_to_list(int d[], int size)
+{
+    list *head = create_list(d[0]);
+    for(int i = 1; i < size; i++)
+    {
+        head = add_to_front(d[i], head);
+    }
+    return head;
+}
+
+void concat(list *h1, list *h2)
+{
+    if(h1 -> next == NULL)
+        h1 -> next = h2;
+    else
+        concat(h1 -> next, h2);
+}
+
+int count_iter(list *h)
+{
+    int i = 0;
+    while(h != NULL)
+    {
+        ++i;
+        h = h -> next;
+    }
+    return i;
+}
+
+int count_rec(list *h)
+{
+    if(h == NULL)
+        return 0;
+    else
+        return (1 + count_rec(h -> next));
+}
+
 void print_list(list *h, char *tittle)
 {
     printf("%s\n", tittle);
@@ -39,23 +76,43 @@ void print_list(list *h, char *tittle)
         printf("%d :", h -> data);
         h = h -> next;
     }
+    printf("\n\n");
 }
 
 int main(void)
 {
-    /* code */
-    list list_of_int;
+    /* list example */
     list *head = NULL;
     list *tail = 0;
 
     /* dynamic space allcation */
     head = malloc(sizeof(list));
-    printf("sizeof(list) is: %lu bytes.\n", sizeof(list));
+    printf("Understandind malloc idiom\nsizeof(list) is: %lu bytes.\n\n",
+           sizeof(list));
 
+    /* load data */
     head -> data = 5;
     head -> next = NULL;
-
     print_list(head, "my list");
 
+    /* exapandind its uses */
+    list *s_list = NULL, *n_list;
+    
+    int data[] = {2,3,5,7};
+    n_list = array_to_list(data, 4);
+    print_list(n_list, "from array list");
+
+    printf("is empty? %d\n", is_empty(s_list));
+
+    s_list = create_list(5);
+    s_list = add_to_front(2, s_list);
+    s_list = add_to_front(4, s_list);
+    printf("is empty? %d\n", is_empty(s_list));
+    printf("Count is %d\n", count_iter(s_list));
+    printf("Count is %d\n", count_rec(s_list));
+    print_list(s_list, "better list");
+
+    concat(s_list, n_list);
+    print_list(s_list, "concat");
     return 0;
 }
