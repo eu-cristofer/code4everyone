7fb55a9e7ccdccde46b7291524815874c609fd28
Cristofer Costa <cristofercosta@yahoo.com.br>
2023-05-08 10:11:19 -0300

    restoring the last files of the structured programming module

---
+++ 01_structured_programming/20_miles_to_km.cpp
@@ -0,0 +1,28 @@
+
+/*
+    Code Title
+
+    Cristofer Costa
+    cristofercosta@yahoo.com.br
+*/
+
+#include <iostream>
+
+using namespace :: std;
+const double m_to_k = 1.609;
+
+inline double convert(int mi){return(mi * m_to_k);}
+
+int main(void) {
+    
+    /* code */
+    int miles = 1;
+    while (miles != 0)
+    {
+        cout << "Input distance in miles or 0 to terminate: ";
+        cin >> miles;
+        cout << "\nDistance is " << convert(miles) << "km." << endl;
+    }
+    cout << endl;    
+    return 0;
+}
