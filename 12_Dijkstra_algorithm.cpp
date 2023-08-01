4bb99ab0cc470b04cb9fde460c7031b73ece1d25
eu-cristofer <cristofercosta@yahoo.com.br>
2023-08-01 16:33:37 -0300

    Changed nomenclature from: node to: vertex

--- 12_Dijkstra_algorithm.cpp
+++ 12_Dijkstra_algorithm.cpp
@@ -18,51 +18,52 @@ using namespace std;
 // Class to store the Edge data. 
 class Edge {
 public:
-    unsigned short _cost;           // Edge positive cost (distance)
     unsigned short _v;              // Vertex v - origin
     unsigned short _w;              // Vertex w - destination
+    unsigned short _cost;           // Edge positive cost (distance)
 
-    Edge(unsigned short cost,
-         unsigned short v,
-         unsigned short w) : _cost(cost),
-                             _v(v),
-                             _w(w) {}
+    Edge(unsigned short v,
+         unsigned short w,
+         unsigned short cost) : _v(v),
+                                _w(w),
+                                _cost(cost) {}
 };
 
 
-// Class to implement a Node wrapper
-class Node {
+// Class to implement a Vertex wrapper
+class Vertex {
 private:
     list<Edge*> _edges;             // List of edges        
-    unsigned short _id;             // Node id
+    unsigned short _id;             // Vertex id
 
 public:
     // Constructor
-    Node(unsigned short id);
+    Vertex(unsigned short id);
 
     // Destructor
-    ~Node();
+    ~Vertex();
 
-    // Add a new edge to the Node
-    void add_edge(unsigned short cost,
-                  unsigned short v,
-                  unsigned short w);
+    // Add a new edge to the Vertex
+    void add_edge(unsigned short v,
+                  unsigned short w,
+                  unsigned short cost);
 
-    // Print out all edges from the Node
+    // Print out all edges of the Vertex
     void print_edges();
 
-    // Return the number of edges from the Node
+    // Return the number of edges from the Vertex
     int edges_size(){return _edges.size();};
 };
 
-Node::Node(unsigned short id): _id(id) {
+Vertex::Vertex(unsigned short id): _id(id) {
     // Constructor
-    // cout << "New Node #" << id << " object instantiated." << endl;
+    cout << "New Vertex #" << id << " object instantiated." << endl;
 }
 
-Node::~Node(){
-    // Delete all Edges when the Node is destroyed
-    cout << "Destroing Node #" << _id << " object" << endl;
+Vertex::~Vertex(){
+    // Destructor
+    // Delete all Edges when the Vertex is destroyed
+    cout << "Destroing Vertex #" << _id << " object" << endl;
     list<Edge*>::iterator it;
     for (it = _edges.begin(); it != _edges.end(); it++){
         cout << "Destroing Edge (v: " << (*it)->_v;
@@ -71,18 +72,18 @@ Node::~Node(){
     }
 }
 
-void Node::add_edge(unsigned short cost,
-                    unsigned short v,
-                    unsigned short w) {
-    // Create a new Edge fo the Node
-    Edge* new_edge = new Edge(cost, v, w);
+void Vertex::add_edge(unsigned short v,
+                      unsigned short w,
+                      unsigned short cost) {
+    // Create a new Edge for the Vertex
+    Edge* new_edge = new Edge(v, w, cost);
     _edges.push_back(new_edge);
     // cout << "New edge created." << endl;
 }
 
-void Node::print_edges(){
-    // Print all edges conected to the Node
-    cout << "Node id #" << _id << ":" << endl;
+void Vertex::print_edges(){
+    // Print all edges conected to the Vertex
+    cout << "Vertex id #" << _id << ":" << endl;
     list<Edge*>::iterator it;
     for (it = _edges.begin(); it != _edges.end(); it++){
         cout << "\t" << "Edge (v: " << (*it)->_v;
@@ -96,107 +97,107 @@ void Node::print_edges(){
 class Graph
 {
 private:
-    vector<Node*> _nodes;
+    vector<Vertex*> _vertices;
 public:
     // Constructor
-    Graph(/* args */);
+    Graph();
 
     // Destructor
     ~Graph();
 
-    // Return the number of Nodes of the Graph
-    int nodes_size(){return _nodes.size();};
+    // Return the number of Vertcies of the Graph
+    int vertices_size(){return _vertices.size();};
 
     // Return the number of Edges of the Graph
     int edges_size();
 
-    // Add node to the Graph
-    void add_node(Node* node_ptr){_nodes.push_back(node_ptr);};
+    // Add Vertex to the Graph
+    void add_vertex(Vertex* vertex_ptr){_vertices.push_back(vertex_ptr);};
 
-    // Print Nodes and its Edges from the Graph
-    void print_nodes();
+    // Print all Vertices and its Edges from the Graph
+    void print_vertices();
 
     // Generate random data
-    void random_graph(unsigned short nodes,
+    void random_graph(unsigned short vertices_size,
                       double edge_density,
                       unsigned short distance_range);
 };
 
-Graph::Graph(/* args */){
+Graph::Graph(){
     // Constructor
-    // cout << "New Graph object instantiated." << endl;
+    cout << "New Graph object instantiated." << endl;
 }
 
 Graph::~Graph(){
     cout << "Destroing Graph object" << endl;
-    vector<Node*>::iterator it;
-    for (it=_nodes.begin(); it!=_nodes.end(); it++){
+    vector<Vertex*>::iterator it;
+    for (it=_vertices.begin(); it!=_vertices.end(); it++){
         delete *it;
     }
 }
 
 int Graph::edges_size(){
+    // Return the number of Edges of the Graph
     int sum = 0;
-    vector<Node*>::iterator it;
-    for (it=_nodes.begin(); it!=_nodes.end(); it++){
+    vector<Vertex*>::iterator it;
+    for (it=_vertices.begin(); it!=_vertices.end(); it++){
         sum += (*it)->edges_size();
     }
     return sum;
 }
 
-void Graph::print_nodes(){
-    // Print Nodes and its Edges from the Graph
-    vector<Node*>::iterator it;
-    for (it=_nodes.begin(); it!=_nodes.end(); it++){
+void Graph::print_vertices(){
+    // Print Vertices and its Edges from the Graph
+    vector<Vertex*>::iterator it;
+    for (it=_vertices.begin(); it!=_vertices.end(); it++){
         (*it)->print_edges();
     }
 }
 
-void Graph::random_graph(unsigned short nodes,
+void Graph::random_graph(unsigned short vertices_size,
                          double edge_density,
                          unsigned short distance_range){
     // This function aims to create a random graph
-    Node* node;
+    Vertex* vertex;
     default_random_engine generator(time(0));
-    uniform_int_distribution<unsigned> random_nodes(1, nodes),
+    uniform_int_distribution<unsigned> random_vertices(0, vertices_size-1),
                                        random_edges(0,4),
                                        random_distance(1, distance_range);
     int cost, destination;
-    cout << "\nrandom Graph being generated.\n";
-    for (int i = 0; i < nodes; i++){
-        node = new Node(i);
+    cout << "\nRandom Graph being generated.\n";
+    for (int i = 0; i < vertices_size; i++){
+        vertex = new Vertex(i);
         for (int j = random_edges(generator); j < 5; j++){
             cost = random_distance(generator);
-            node->add_edge(cost, i, random_nodes(generator));
+            vertex->add_edge(i, random_vertices(generator), cost);
         } 
-        add_node(node);
+        add_vertex(vertex);
     }
 }
 
 
-int main(int argc, const char * argv[]) {
-    
+int main() {
     /* Manual Graph creation */ 
     cout << "\nManual Graph creation:\n";   
     Graph graph;
-    Node* node_ptr = new Node(1);
-    node_ptr->add_edge(1,1,2), node_ptr->add_edge(2,1,3);
-    cout << "\nPrinting all the edges from a node:\n";
-    node_ptr->print_edges();
+    Vertex* vertex_ptr = new Vertex(1);
+    vertex_ptr->add_edge(1,3,2), vertex_ptr->add_edge(3,5,3);
+    cout << "\nPrinting all the edges from a vertex:\n";
+    vertex_ptr->print_edges();
     cout << "\nPrinting additional node information:\n";
-    cout << "Edges size: " << node_ptr->edges_size() << endl;
-    cout << "\nAdding a node manually to a Graph object:\n";
-    graph.add_node(node_ptr);
-    cout << "Graph size: " << graph.nodes_size() << " node(s).\n\n";
+    cout << "Edges size: " << vertex_ptr->edges_size() << endl;
+    cout << "\nAdding a vertex manually to a Graph object:\n";
+    graph.add_vertex(vertex_ptr);
+    cout << "Graph size: " << graph.vertices_size() << " vertex(s).\n\n";
 
     /* Automatic Graph creation*/
     cout << "\n\nAutomatic Graph creation:\n";
     Graph graph_random;
     graph_random.random_graph(10,1,10);
     cout << "\nPrinting Graph information:\n";
-    cout << "Nodes size: " << graph_random.nodes_size() << endl;
+    cout << "Vertices size: " << graph_random.vertices_size() << endl;
     cout << "Edges size: " << graph_random.edges_size() << endl;
-    graph_random.print_nodes();
+    graph_random.print_vertices();
 
     return 0;
 }
